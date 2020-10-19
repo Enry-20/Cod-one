c3b8d649e88ab7150a561733da92f6d6acea7644
Enry-20 <enriquecuellar7u7@gmail.com>
2020-10-19 03:21:26 -0500

    first-commit

---
+++ animation.js
@@ -0,0 +1,7 @@
+let enlace= document.getElementsByClassName("liga1");
+function preesme(){
+  window.location.href="https://getbootstrap.com/docs/4.5/components/collapse/"
+}
+
+enlace.onclick= preesme;
+
